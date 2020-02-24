ccec18f66f10f624058765cc1e8b36e3376d3de8
Vitali <vskrabatun@gmail.com>
2020-02-24 23:28:19 +0300

    fix: menu(order-list) and add event delegation

--- assets/js/App.js
+++ assets/js/App.js
@@ -3,20 +3,23 @@ import { getMarkupAllOrdersInList } from './get-markup-all-orders-in-list';
 import { showDetailsOrder } from './show-details-order';
 import { addEventListeners } from './add-event-listeners';
 
+window.addEventListener('DOMContentLoaded', init);
+
 function init() {
-  if (window.innerWidth < 1075) {
-    document.querySelector('.order-list').classList.add('order-list--hidden');
-    document.querySelector('.order-list').style.display = 'none';
-    setTimeout(() => document.querySelector('.order-list').style.display = 'block', 500);
+  const tabletWidth = 1075;
+
+  if (window.innerWidth < tabletWidth) { // removing menu animation (leaving to left), when user just opened the page
+    const menu = document.querySelector('.order-list');
+    menu.classList.add('order-list--hidden');
+    menu.style.display = 'none';
+    setTimeout(() => menu.style.display = 'block', 0);
   }
 
   const orderListMain = document.querySelector('.order-list__main');  
   orderListMain.innerHTML = getMarkupAllOrdersInList(Orders);
 
-  document.querySelectorAll('.order-list__item')[1].classList.add('order-list__item--selected');
+  document.querySelectorAll('.order-list__item')[1].classList.add('order-list__item--selected');  // default selected order
   showDetailsOrder(document.querySelector('.order-list__item--selected'), Orders);
 
   addEventListeners();  
 }
-
-window.addEventListener('DOMContentLoaded', init);
--- assets/js/add-event-listeners.js
+++ assets/js/add-event-listeners.js
@@ -4,22 +4,23 @@ import { searchOrders } from './search-orders';
 import { sortingProducts } from './sorting-products';
 
 export function addEventListeners() {
+  let sortingDirection = 'from-minimum';
+  const tabletWidth = 1075;
+
   const buttonBack = document.querySelector('.order-list__button-back');
   const headerButton = document.querySelector('.header__button');
 
   const contentWrapper = document.querySelector('.content-wrapper');
-  const orderList = document.querySelector('.order-list');  
+  const orderList = document.querySelector('.order-list');
+  const orderItemsMain = document.querySelector('.order-list__main');  
 
   const buttonAddress = document.querySelector('.order__button-address');
   const buttonProcessor = document.querySelector('.order__button-processor');
   const sectionOrderAddress = document.querySelector('section.order__address');
   const sectionOrderProcessor = document.querySelector('section.order__processor');
 
-  const orderItems = document.querySelectorAll('.order-list__item');
-
   const searchFromOrdersList = document.querySelector('.order-list__form input');
   const imagesOfSortingProducts = document.querySelectorAll('.order__line-list-row--headline img');
-  let sortingDirection = 'from-minimum';
 
   buttonBack.addEventListener('click', () => {
     contentWrapper.classList.add('content-wrapper--menu-hidden');
@@ -43,18 +44,25 @@ export function addEventListeners() {
     sectionOrderProcessor.classList.toggle('order__processor--hidden');
   });
 
-  orderItems.forEach((item) => {
-    item.addEventListener('click', () => {
-      // remove class from last selected
-      document.querySelector('.order-list__item--selected').classList.remove('order-list__item--selected');
+  orderItemsMain.addEventListener('click', (e) => {
+    if (!e.target.classList.contains('order-list__main')) { //  event delegation      
+      let orderListItem = e.target;
 
-      item.classList.add('order-list__item--selected');
-      showDetailsOrder(item, Orders); // ! Переписать в делегирование сделать один обработчик для order-list__main
-    
-      if (window.innerWidth < 1075) { // ! magic number
+      while (!orderListItem.classList.contains('order-list__item')) { //find clicked order
+        orderListItem = orderListItem.parentNode;
+      }
+
+      if (document.querySelector('.order-list__item--selected')) {
+        document.querySelector('.order-list__item--selected').classList.remove('order-list__item--selected');
+      }
+
+      orderListItem.classList.add('order-list__item--selected');
+      showDetailsOrder(orderListItem, Orders);
+
+      if (window.innerWidth < tabletWidth) {
         buttonBack.click(); // hide the menu when the user selects an order
       }
-    });
+    }
   });
 
   searchFromOrdersList.addEventListener('input', () => {
